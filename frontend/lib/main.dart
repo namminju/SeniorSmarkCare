4b7d482e0a6b04f4bad9b4cf3ccd0dd32216b2c3
mj <nampink2300@gmail.com>
2024-05-28 06:45:13 +0900

    [FE] 메인 페이지 및 Symtom, Medical 페이지 수정

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screen/MainScreen.dart';
+import 'screen/MainScreen.dart';
 
 void main() {
   runApp(const MyApp());
--- frontend/lib/screen/SymtomPage/SymtomCategory.dart
+++ frontend/lib/screen/SymtomPage/SymtomCategory.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import '../../widgets/PageNavigationBigButton.dart';
+import './SymtomHistory.dart';
 
 class SymtomCategory extends StatefulWidget {
+  final VoidCallback? onSubmit;
+
+  SymtomCategory({Key? key, this.onSubmit}) : super(key: key);
+
   @override
   _SymtomCategoryState createState() => _SymtomCategoryState();
 }
@@ -19,6 +25,10 @@ class _SymtomCategoryState extends State<SymtomCategory> {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size; // 반응형으로 구현하기 위함
+    double width = screenSize.width;
+    double height = screenSize.height; // 상대 수치를 이용하기 위함
+
     return AlertDialog(
       insetPadding: EdgeInsets.zero,
       backgroundColor: Color(0xFFF0F0F0),
@@ -52,32 +62,268 @@ class _SymtomCategoryState extends State<SymtomCategory> {
                         width: 295,
                         height: 90,
                         child: Center(
-                          child: Text(
-                            getButtonText(i),
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: width * 0.16,
+                                height: width * 0.16,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFFEB2B2),
+                                    border: Border.all(
+                                        color: Colors.black,
+                                        width: width * 0.01),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Center(
+                                    child: Image.asset(
+                                      getButtonImage(i),
+                                      width: width * 0.11,
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              SizedBox(width: 5), // 간격 조절
+                              Text(
+                                getButtonText(i),
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '   +   ',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ),
                   ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFEB2B2), // 버튼의 배경색을 빨간색으로 지정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    elevation: 4,
+                    minimumSize: Size(280, 40),
+                    maximumSize: Size(300, 45),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                          content: Container(
+                            width: 288, // 원하는 너비 설정
+                            height: 256, // 원하는 높이 설정
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '알림',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        // 취소 버튼을 누르면 다이얼로그만 종료
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('X'),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4),
+                                ),
+                                Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.all(24),
+                                ),
+                                Text(
+                                  '제출이 완료되었습니다.',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(24),
+                                ),
+                                // PageNavigationBigButton 위젯 생성하면서 텍스트와 nextPage 설정
+                                PageNavigationBigButton(
+                                  buttonText: '확인하러 가기',
+                                  nextPage:
+                                      SymtomHistory(), // MedicalHistory 페이지로 이동
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: Text(
+                    '제출',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               ],
             )
           else
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red, // 버튼의 배경색을 빨간색으로 지정
-              ),
-              onPressed: () {
-                setState(() {
-                  currentPageIndex = 0;
-                });
-              },
-              child: Text('첫 번째 페이지로 돌아가기'),
-            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFEB2B2), // 버튼의 배경색을 빨간색으로 지정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    elevation: 4,
+                    minimumSize: Size(110, 40),
+                    maximumSize: Size(130, 45),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      currentPageIndex = 0;
+                    });
+                  },
+                  child: Text(
+                    '이전',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFEB2B2), // 버튼의 배경색을 빨간색으로 지정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    elevation: 4,
+                    minimumSize: Size(110, 40),
+                    maximumSize: Size(130, 45),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                          content: Container(
+                            width: 288, // 원하는 너비 설정
+                            height: 256, // 원하는 높이 설정
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '알림',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        // 취소 버튼을 누르면 다이얼로그만 종료
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('X'),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4),
+                                ),
+                                Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.all(24),
+                                ),
+                                Text(
+                                  '제출이 완료되었습니다.',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(24),
+                                ),
+                                // PageNavigationBigButton 위젯 생성하면서 텍스트와 nextPage 설정
+                                PageNavigationBigButton(
+                                  buttonText: '확인하러 가기',
+                                  nextPage:
+                                      SymtomHistory(), // MedicalHistory 페이지로 이동
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: Text(
+                    '제출',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            )
         ],
       ),
     );
@@ -100,6 +346,21 @@ class _SymtomCategoryState extends State<SymtomCategory> {
   }
 }
 
+String getButtonImage(int index) {
+  switch (index) {
+    case 1:
+      return 'images/symtomImg/head.png';
+    case 2:
+      return 'images/symtomImg/upper.png';
+    case 3:
+      return 'images/symtomImg/lower.png';
+    case 4:
+      return 'images/symtomImg/handfoot.png';
+    default:
+      return '';
+  }
+}
+
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -118,6 +379,9 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   bool isChecked1 = false;
   bool isChecked2 = false;
+  bool isChecked3 = false;
+  bool isChecked4 = false;
+  bool isChecked5 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -127,56 +391,75 @@ class _SecondPageState extends State<SecondPage> {
         Text(
           '머리',
           style: TextStyle(
-            fontSize: 28,
+            fontSize: 32,
             fontWeight: FontWeight.bold,
           ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 30),
         ),
         CheckboxListTile(
           title: Text('통증 1'),
           value: isChecked1,
+          activeColor: Color(0xFFFEB2B2),
           onChanged: (bool? value) {
             setState(() {
               isChecked1 = value ?? false;
             });
           },
         ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
+        ),
         CheckboxListTile(
           title: Text('통증 2'),
           value: isChecked2,
+          activeColor: Color(0xFFFEB2B2), // 체크된 상태의 색상을 초록색으로 변경
           onChanged: (bool? value) {
             setState(() {
               isChecked2 = value ?? false;
             });
           },
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
         ),
         CheckboxListTile(
           title: Text('통증 3'),
-          value: isChecked2,
+          value: isChecked3,
+          activeColor: Color(0xFFFEB2B2), // 체크된 상태의 색상을 빨간색으로 변경
           onChanged: (bool? value) {
             setState(() {
-              isChecked2 = value ?? false;
+              isChecked3 = value ?? false;
             });
           },
         ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
+        ),
         CheckboxListTile(
-          title: Text('통증4 '),
-          value: isChecked2,
+          title: Text('통증 4'),
+          value: isChecked4,
+          activeColor: Color(0xFFFEB2B2), // 체크된 상태의 색상을 주황색으로 변경
           onChanged: (bool? value) {
             setState(() {
-              isChecked2 = value ?? false;
+              isChecked4 = value ?? false;
             });
           },
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
         ),
         CheckboxListTile(
           title: Text('통증 5'),
-          value: isChecked2,
+          value: isChecked5,
+          activeColor: Color(0xFFFEB2B2), // 체크된 상태의 색상을 보라색으로 변경
           onChanged: (bool? value) {
             setState(() {
-              isChecked2 = value ?? false;
+              isChecked5 = value ?? false;
             });
           },
         ),
-
         // Add more CheckboxListTile widgets here if needed
       ],
     );
